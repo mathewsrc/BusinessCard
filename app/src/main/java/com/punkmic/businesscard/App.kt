0b894e38d1a2dad3c7efb35252052351154c2876
Punkmic <mattheus_ribeiro@outlook.com>
2022-07-23 17:12:56 -0300

    Add Hilt library to inject dependencies.

--- app/src/main/java/com/punkmic/businesscard/App.kt
+++ app/src/main/java/com/punkmic/businesscard/App.kt
@@ -1,6 +1,7 @@
 package com.punkmic.businesscard
 
 import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
 
-class App:Application() {
-}
+@HiltAndroidApp
+class App : Application()
